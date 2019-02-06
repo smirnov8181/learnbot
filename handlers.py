17d09352d08ae5686db1af0549c807100dcf7353
Anton Smirnov <smirnov8181@yandex.ru>
2019-02-06 18:48:16 +0300

    Add MongoDB func

--- handlers.py
+++ handlers.py
@@ -3,51 +3,60 @@ import os
 import logging
 from random import choice
 
-from telegram import ReplyKeyboardRemove, ReplyKeyboardMarkup, ParseMode
+from telegram import ReplyKeyboardRemove, ReplyKeyboardMarkup, ParseMode, error
 from telegram.ext import ConversationHandler
 from telegram.ext import messagequeue as mq
 
-from utils import get_keyboard, get_user_emo, is_cat
-
 from bot import subscribers
+from db import db, get_or_create_user, get_user_emo, toggle_subscription, get_subscribers
+from utils import get_keyboard, is_cat
+
+
 
 
 
 def greet_user(bot, update, user_data):
-    print(update.message.chat_id)
-    emo = get_user_emo(user_data)
+    user = get_or_create_user(db, update.effective_user, update.message)
+    emo = get_user_emo(db, user)
     user_data['emo'] = emo
     text = "Привет, {}!".format(emo)
     update.message.reply_text(text, reply_markup=get_keyboard())
 
 def get_contact(bot, update, user_data):
+    user = get_or_create_user(db, update.effective_user, update.message)
     logging.info(update.message.contact)
-    update.message.reply_text('Готово: {}'.format(get_user_emo(user_data)), reply_markup=get_keyboard())
+    update.message.reply_text('Готово: {}'.format(get_user_emo(db, user)), reply_markup=get_keyboard())
 
 def get_location(bot, update, user_data):
+    user = get_or_create_user(db, update.effective_user, update.message)
     logging.info(update.message.contact)
-    update.message.reply_text('Готово: {}'.format(get_user_emo(user_data)), reply_markup=get_keyboard())
+    update.message.reply_text('Готово: {}'.format(get_user_emo(db, user)), reply_markup=get_keyboard())
 
 def talk_to_me(bot, update, user_data):
-    emo = get_user_emo(user_data)
-    user_text = "Привет, {} {}! Ты написал: {}".format(update.message.chat.first_name, user_data['emo'], 
+    user = get_or_create_user(db, update.effective_user, update.message)
+    emo = get_user_emo(db, user)
+    user_text = "Привет, {} {}! Ты написал: {}".format(user['first_name'], emo, 
                 update.message.text)
-    logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username, 
+    logging.info("User: %s, Chat id: %s, Message: %s", user['username'], 
                 update.message.chat.id, update.message.text)
     update.message.reply_text(user_text, reply_markup=get_keyboard())
 
 def send_cat_picture(bot, update, user_data):
+    user = get_or_create_user(db, update.effective_user, update.message)
     cat_list = glob('images/cat*.jp*g')
     cat_pic = choice(cat_list)
     bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, 'rb'), reply_markup=get_keyboard())
 
 def change_avatar(bot, update, user_data):
+    user = get_or_create_user(db, update.effective_user, update.message)
+    emo = get_user_emo(db, user)
     if 'emo' in user_data:
-        del user_data['emo']
-    emo = get_user_emo(user_data)
+        del user['emo']
+    emo = get_user_emo(db, user)
     update.message.reply_text('Готово: {}'.format(emo), reply_markup=get_keyboard())
 
 def check_user_photo(bot, update, user_data):
+    user = get_or_create_user(db, update.effective_user, update.message)
     update.message.reply_text("Обрабатываю фото")
     os.makedirs('downloads', exist_ok=True)
     photo_file = bot.getFile(update.message.photo[-1].file_id)
@@ -63,10 +72,12 @@ def check_user_photo(bot, update, user_data):
         update.message.reply_text("Тревога, котик не обнаружен!")
 
 def anketa_start(bot, update, user_data):
+    user = get_or_create_user(db, update.effective_user, update.message)
     update.message.reply_text("Как вас зовут? Напишите имя и фамилию", reply_markup=ReplyKeyboardRemove())
     return "name"
 
 def anketa_get_name(bot, update, user_data):
+    user = get_or_create_user(db, update.effective_user, update.message)
     user_name = update.message.text
     if len(user_name.split(" ")) !=2:
         update.message.reply_text("Введите Имя и Фамилию")
@@ -78,12 +89,14 @@ def anketa_get_name(bot, update, user_data):
         return "rating"
 
 def anketa_rating(bot, update, user_data):
+    user = get_or_create_user(db, update.effective_user, update.message)
     user_data['anketa_rating'] = update.message.text
     update.message.reply_text("""Пожалуйста напишите отзыв в свободной форме 
 или /cancel чтобы пропустить этот шаг""")
     return "comment"
 
 def anketa_comment(bot, update, user_data):
+    user = get_or_create_user(db, update.effective_user, update.message)
     user_data["anketa_comment"] = update.message.text
     text = """ 
 <b>Имя Фамилия:</b> {anketa_name}
@@ -93,6 +106,7 @@ def anketa_comment(bot, update, user_data):
     return ConversationHandler.END
 
 def anketa_skip_comment(bot, update, user_data):
+    user = get_or_create_user(db, update.effective_user, update.message)
     text = """ 
 <b>Имя Фамилия:</b> {anketa_name}
 <b>Оценка:</b> {anketa_rating}""".format(**user_data)
@@ -100,24 +114,30 @@ def anketa_skip_comment(bot, update, user_data):
     return ConversationHandler.END
 
 def dontknow(bot, update, user_data):
+    user = get_or_create_user(db, update.effective_user, update.message)
     update.message.reply_text("Не понимаю")
 
 def subscribe(bot, update):
-    subscribers.add(update.message.chat_id)
+    user = get_or_create_user(db, update.effective_user, update.message)
+    if not user.get('subscribed'):
+        toggle_subscription(db, user)
     update.message.reply_text('Вы подписались')
-    print(subscribers)
 
 @mq.queuedmessage
 def send_updates(bot, job):
-    for chat_id in subscribers:
-        bot.sendMessage(chat_id=chat_id, text='Куку')
+    for user in get_subscribers(db):
+        try: 
+            bot.sendMessage(chat_id=user['chat_id'], text='Куку')
+        except error.BadRequest:
+            print('Chat {} not found'.format(user['chat_id']))
 
 def unsubscribe(bot, update):
-    if update.message.chat_id in subscribers:
-        subscribers.remove(update.message.chat_id)
+    user = get_or_create_user(db, update.effective_user, update.message)
+    if user.get('subscribed'):
+        toggle_subscription(db, user)
         update.message.reply_text('Вы отписались')
     else:
-        update.message.reply_text('Вы не подписаны')
+        update.message.reply_text('Вы не подписаны, нажмите /subscribe чтобы подписаться')
 
 def set_alarm(bot, update, args, job_queue):
     try:
